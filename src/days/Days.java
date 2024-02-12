c9cbe3d2ce3376d1bec4dae4c743b3e59d9b01ae
User <User@LAPTOP-PL1PHOF3>
2024-02-12 09:40:54 -0500

    I have added a new branch and modified the code

--- src/days/Days.java
+++ src/days/Days.java
@@ -16,20 +16,42 @@ import java.util.Scanner;
  *
  * @author sivagamasrinivasan
  jn 22nd
+ * 
+ * modified by: Bhadra Sreeja Nair
+ * Student ID: 991716665
  */
+package days;
+
+import java.util.Scanner;
+
+enum Day {
+    ONE("Monday"),
+    TWO("Tuesday"),
+    THREE("Wednesday"),
+    FOUR("Thursday"),
+    FIVE("Friday"),
+    SIX("Saturday"),
+    SEVEN("Sunday");
+
+    private final String name;
+
+    Day(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
 public class Days {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-         
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the day number (one,two,three,four,five) in string");
-    String code = in.next();
-    Weekdays t= new Weekdays();
-    t.nameOfDay(code); 
-    }// TODO code application logic here
-    
-    
+        Scanner in = new Scanner(System.in);
+
+        // Print all days and names
+        for (Day day : Day.values()) {
+            System.out.println(day.name() + ": " + day.getName());
+        }
+    }
 }
